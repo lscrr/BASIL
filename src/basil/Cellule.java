7cb16ecae61bcf6808d80be135a7d7e2bcae5b3e
titou <titou@LAPTOP-B0AVNMH5>
2022-03-17 13:00:58 +0100

    début des méthodes dans les classes cellule, carte et joueur

--- src/basil/Cellule.java
+++ src/basil/Cellule.java
@@ -9,5 +9,62 @@ package basil;
  * @author titou
  */
 public class Cellule {
+    Carte CarteCourante;
+    boolean Cartes;
+    boolean Joker;
+    boolean JokPasNul;
+    boolean JokNul;
+    public Cellule (){
+        CarteCourante= null;
+        Joker = false;
+        JokPasNul=false;
+        JokNul= false;
+    }
+    public boolean affecterCarte(Carte c){
+        if (CarteCourante != null){
+            CarteCourante =c;
+            return true;
+        } else{
+            return false;
+        }
+    }
+    public boolean PrésenceCarte(){
+        if (Cartes == true){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public boolean présenceJoker(){
+         if (Joker  == true){
+            return true;
+        }else{
+            return false;
+        }
+    }
     
+    public boolean présenceJokNul(){
+         if (JokNul == true){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public boolean présenceJokPasNul(){
+        if (JokPasNul == true){
+            return true;
+        }else{
+            return false;
+        } 
+    }
+    
+    public void retournerCarte (int x, int y ){
+        
+    }
+    
+    public String lireImage(){
+        
+    }
 }
+
