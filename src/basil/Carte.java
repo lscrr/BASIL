a3055fb2750f387fc6893d30c697a20daee82f5a
titou <titou@LAPTOP-B0AVNMH5>
2022-03-17 12:01:41 +0100

    Création des classes

---
+++ src/basil/Carte.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package basil;
+
+/**
+ *
+ * @author titou
+ */
+public class Carte {
+    int cartesPaires;
+    int cartesTriples;
+    int nombreJoker;
+    int nombreJokPasNul;
+    int nombreJokNul;
+    public Carte (String Image){
+        
+    }
+}
+
