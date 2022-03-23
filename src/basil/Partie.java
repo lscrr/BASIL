2c4c34794201bb321135cc4faed42ae123f37888
titou <titou@LAPTOP-B0AVNMH5>
2022-03-23 14:47:03 +0100

    modification méthode partie

--- src/basil/Partie.java
+++ src/basil/Partie.java
@@ -6,8 +6,30 @@ package basil;
 
 /**
  *
- * @author titou
+ * @author lisa et baptiste
  */
 public class Partie {
+    
+    Joueur [] ListeJoueurs= new Joueur[2];
+    Joueur joueurCourant;
+    Grille grilleJeu;
+    int valeurDe;
+    
+    public Partie (Joueur J1, Joueur J2){
+        ListeJoueurs[0] = J1;
+        ListeJoueurs [1]= J2;
+        
+    }
+    public int lancerDe(){  // permet de prendre en compte la valeur du dé
+        valeurDe=0;
+        valeurDe= (int)(1+ 6*Math.random());
+        return valeurDe;
+    }
+    
+    public void intialiserPartie(){
+        grilleJeu= new Grille ();
+        System.out.println(joueurCourant+ " Lancer le dé");
+        joueurCourant
+    }
     
 }
